98e472cd5f063c19e13f767a5b1081370585bf52
Maxim <maxrap7878@mail.ru>
2018-07-17 11:20:23 +0300

    final version

---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+
+const server = new WebSocket.Server({ port: 3000});
+
+
+server.on('connection', ws =>{
+    ws.send('Добро пожаловать в MaxChart');
+    ws.on('message', message =>{
+        if (message === 'exit'){
+            ws.close()
+        }else{
+            server.clients.forEach(client =>{
+                if (client.readyState === WebSocket.OPEN ){
+                    client.send(message);
+                }
+            })
+        }
+
+    });
+
+});
